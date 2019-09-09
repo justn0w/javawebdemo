77a345e6da51295e1aa4c5e8c25038e23da3f6ec
justnow <ggjustnow@163.com>
2019-09-09 15:48:44 +0800

    Spring框架总结(三)——bean标签和管理对象细节(注入集合)

---
+++ spring/Spring_demo2/src/main/java/com/justnow/service/impl/UserServiceImpl2.java
@@ -0,0 +1,43 @@
+package com.justnow.service.impl;
+
+import com.justnow.service.IUserService;
+
+import java.util.*;
+
+public class UserServiceImpl2 implements IUserService {
+
+    private String[] myStrs;
+    private List<String> myList;
+    private Set<String> mySet;
+    private Map<String, String> myMap;
+    private Properties myProps;
+
+    public void setMyStrs(String[] myStrs) {
+        this.myStrs = myStrs;
+    }
+
+    public void setMyList(List<String> myList) {
+        this.myList = myList;
+    }
+
+    public void setMySet(Set<String> mySet) {
+        this.mySet = mySet;
+    }
+
+    public void setMyMap(Map<String, String> myMap) {
+        this.myMap = myMap;
+    }
+
+    public void setMyProps(Properties myProps) {
+        this.myProps = myProps;
+    }
+
+
+    public void save() {
+        System.out.println(Arrays.toString(myStrs));
+        System.out.println(myList);
+        System.out.println(mySet);
+        System.out.println(myMap);
+        System.out.println(myProps);
+    }
+}
